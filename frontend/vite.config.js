f87dd5ed05576bca96b37c7d8596943ec7e10f74
Mr.X <ravis9155499@gmail.com>
2026-09-06 12:43:09 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: false,   // Disable HTTPS for local dev
+    https: true,   // Disable HTTPS for local dev
     port: 3000,
     open: true,
   },
